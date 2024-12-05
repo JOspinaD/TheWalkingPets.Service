0bae2d67279539a1ddfd1c1be3cd62a2e4d4e46b
Juan Pablo Ospina <juanpabloospina19@gmail.com>
2024-12-04 22:39:21 -0500

    feat: Creation of Dtos for users

--- TheWalkingPets.Service/DTO/Usuario/UsuarioReadDto.cs
+++ TheWalkingPets.Service/DTO/Usuario/UsuarioReadDto.cs
@@ -1,16 +1,16 @@
-﻿using TheWalkingPets.Service.Models.Base;
+﻿using TheWalkingPets.Service.DTO.Base;
 
-namespace TheWalkingPets.Service.Models
+namespace TheWalkingPets.Service.DTO.Usuario
 {
-    public class Usuario : BaseEntity
+    public class UsuarioReadDto : BaseReadDto
     {
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public string NombreUsuario { get; set; }
         public string Correo { get; set; }
         public string HashContraseña { get; set; }
-        public string? Salt {  get; set; }
-        public int? Edad {  get; set; }
+        public string? Salt { get; set; }
+        public int? Edad { get; set; }
         public string? Direccion { get; set; }
         public string? Telefono { get; set; }
     }
--- TheWalkingPets.Service/DTO/Usuario/UsuarioWriteDto.cs
+++ TheWalkingPets.Service/DTO/Usuario/UsuarioWriteDto.cs
@@ -1,16 +1,13 @@
-﻿using TheWalkingPets.Service.Models.Base;
-
-namespace TheWalkingPets.Service.Models
+﻿namespace TheWalkingPets.Service.DTO.Usuario
 {
-    public class Usuario : BaseEntity
+    public class UsuarioWriteDto
     {
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public string NombreUsuario { get; set; }
         public string Correo { get; set; }
         public string HashContraseña { get; set; }
-        public string? Salt {  get; set; }
-        public int? Edad {  get; set; }
+        public int? Edad { get; set; }
         public string? Direccion { get; set; }
         public string? Telefono { get; set; }
     }
