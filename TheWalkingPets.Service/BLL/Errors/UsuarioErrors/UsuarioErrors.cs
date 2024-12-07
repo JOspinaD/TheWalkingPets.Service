35def1e17fd54fae37477ba710bf93d3fa1a355e
Juan Pablo Ospina <juanpabloospina19@gmail.com>
2024-12-06 23:45:36 -0500

    feat: implementation of user error control

---
+++ TheWalkingPets.Service/BLL/Errors/UsuarioErrors/UsuarioErrors.cs
@@ -0,0 +1,22 @@
+﻿using TheWalkingPets.Service.common;
+
+namespace TheWalkingPets.Service.BLL.Errors.UsuarioErrors
+{
+    public class UsuarioErrors
+    {
+        public static readonly Error NotExists = new(
+            "Usuario.NotExists",
+            "Usuario no encontrado"
+            );
+
+        public static readonly Error AlreadyExists = new(
+            "Usuario.AlreadyExists",
+            "El usuario ya existe"
+            );
+
+        public static readonly Error Unhandled = new(
+            "Usuario.Unhandled",
+            "Error no controlado"
+            );
+    }
+}
