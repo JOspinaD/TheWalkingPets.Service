5d5d9ecba79151e119a44d4925fe20e517241ded
juan.Ospina <juan.ospina@vpssas.com>
2025-01-05 22:16:52 -0500

    feat: new function for controllers problemDetails

--- TheWalkingPets.Service/Controllers/UsuarioController.cs
+++ TheWalkingPets.Service/Controllers/UsuarioController.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using TheWalkingPets.Service.BLL.Services.Contract;
+using TheWalkingPets.Service.DTO.Usuario;
 
 namespace TheWalkingPets.Service.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UsuarioController
+    public class UsuarioController(IUsuarioService _service)
     {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UsuarioReadDto>>> GetAll()
+        {
+            var result = await _service.GetAllAsync();
+            return result.IsSuccess ? Ok(result.Value) : result.ToProblemDetails
+        }
     }
 }
