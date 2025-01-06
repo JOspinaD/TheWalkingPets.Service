aea998975a51bc448930ca6dc8d2879e63d0181a
juan.Ospina <juan.ospina@vpssas.com>
2025-01-05 19:37:17 -0500

    feat: Initial conde for controller  user

---
+++ TheWalkingPets.Service/Controllers/UsuarioController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheWalkingPets.Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController
+    {
+    }
+}
