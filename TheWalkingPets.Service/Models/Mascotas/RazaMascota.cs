a2c2995e6ad6eec6dcbc6108b65ae8552552783c
Juan Pablo Ospina <juanpabloospina19@gmail.com>
2025-02-07 17:56:00 -0500

    feat: add Models for Pets

---
+++ TheWalkingPets.Service/Models/Mascotas/RazaMascota.cs
@@ -0,0 +1,13 @@
+﻿using TheWalkingPets.Service.Models.Base;
+
+namespace TheWalkingPets.Service.Models.Mascotas
+{
+    public class RazaMascota : BaseEntity
+    {
+        public string NombreRaza { get; set; }
+        public Guid IdTipoMascota { get; set; }
+
+        public virtual TipoMascota TipoMascota { get; set; }
+        public virtual ICollection<Mascota?> Mascotas { get; set; }
+    }
+}
---
+++ TheWalkingPets.Service/Models/Mascotas/TipoMascota.cs
@@ -0,0 +1,12 @@
+﻿using TheWalkingPets.Service.Models.Base;
+
+namespace TheWalkingPets.Service.Models.Mascotas
+{
+    public class TipoMascota : BaseEntity
+    {
+        public string NombreTipoMascta { get; set; }
+
+        public virtual ICollection<RazaMascota>? RazaMascotas { get; set; }
+        public virtual ICollection<Mascota>? Mascotas { get; set; }
+    }
+}
