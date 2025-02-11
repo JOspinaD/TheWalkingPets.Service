f64e6fcfb78bed6127a4d08d187908c7b212d69b
Juan Pablo Ospina <juanpabloospina19@gmail.com>
2025-02-10 22:19:28 -0500

    feat: add Error control for pets

---
+++ TheWalkingPets.Service/BLL/Errors/MascotaErrors/MascotaErrors.cs
@@ -0,0 +1,31 @@
+﻿using TheWalkingPets.Service.common;
+
+namespace TheWalkingPets.Service.BLL.Errors.MascotaErrors
+{
+    public class MascotaErrors
+    {
+        public static readonly Error NotExists = new(
+            "Mascota.AlreadyExists",
+            "Mascota No encontrada");
+
+        public static readonly Error AlreadyExists = new(
+            "Mascota.AlreadyExists",
+            "La Mascota ya existe");
+
+        public static readonly Error TipoMascotaNotFound = new(
+            "Mascota.TipoMascotaNotFound",
+             "Tipo de Mascota no encontrado");
+
+        public static readonly Error RazaMascotaNotFound = new(
+          "Mascota.RazaMascotaNotFound",
+           "Raza de Mascota no encontrado");
+
+        public static readonly Error UsuarioNotFound = new(
+          "Mascota.UsuarioNotFound",
+            "Usuario no encontrado");
+
+        public static readonly Error Unhandled = new(
+            "Mascota.Unhandled",
+            "Error no controlado");
+    }
+}
---
+++ TheWalkingPets.Service/BLL/Errors/MascotaErrors/RazaMascota.cs
@@ -0,0 +1,24 @@
+﻿using TheWalkingPets.Service.common;
+
+namespace TheWalkingPets.Service.BLL.Errors.MascotaErrors
+{
+    public class RazaMascota
+    {
+        public static readonly Error NotExists = new(
+            "RazaMascota.AlreadyExists",
+            "Raza de Mascota no encontrada");
+
+        public static readonly Error AlreadyExists = new(
+            "RazaMascota.AlreadyExists",
+            "La Raza de Mascota ya existe");
+
+        public static readonly Error TipoMascotaNotFound = new(
+            "RazaMascota.TipoMascotaNotFound",
+             "Tipo de Mascota no encontrado");
+
+        public static readonly Error Unhandled = new(
+            "RazaMascota.Unhandled",
+             "Error no controlado");
+
+    }
+}
