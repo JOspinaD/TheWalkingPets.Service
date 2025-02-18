20440b11e79f2ede62d6e53e33f5811a0d2d60fd
Juan Pablo Ospina <juanpabloospina19@gmail.com>
2025-02-17 21:19:28 -0500

    feat: Add Trabajo Dtos

---
+++ TheWalkingPets.Service/DTO/Trabajo/TrabajoWriteDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TheWalkingPets.Service.DTO.Trabajo
+{
+    public class TrabajoWriteDto
+    {
+        public string Descripcion { get; set; }
+        public DateTime fechaPublicación { get; set; }
+        public Boolean Estado { get; set; }
+        public Guid? IdUsuario { get; set; }
+        public Guid? IdMascota { get; set; }
+    }
+}
