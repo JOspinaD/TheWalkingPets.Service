048297482099b91eba72c7a941655f4e9d3f3d45
Juan Pablo Ospina <juanpabloospina19@gmail.com>
2025-02-17 21:21:50 -0500

    feat: Add UsuarioTrabajoDtos

---
+++ TheWalkingPets.Service/DTO/Trabajo/UsuarioTrabajoReadDto.cs
@@ -0,0 +1,11 @@
+﻿using TheWalkingPets.Service.DTO.Base;
+
+namespace TheWalkingPets.Service.DTO.Trabajo
+{
+    public class UsuarioTrabajoReadDto : BaseReadDto
+    {
+        public Guid? IdUsuario { get; set; }
+        public Guid? IdTrabajo { get; set; }
+        public string Rol { get; set; }
+    }
+}
