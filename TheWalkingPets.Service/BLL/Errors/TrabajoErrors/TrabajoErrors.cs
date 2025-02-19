088354dc2c57f71c89fd911286ab06546ae98165
Juan Pablo Ospina <juanpabloospina19@gmail.com>
2025-02-18 22:21:50 -0500

    feat: Add Errors for Trabajo

---
+++ TheWalkingPets.Service/BLL/Errors/TrabajoErrors/TrabajoErrors.cs
@@ -0,0 +1,19 @@
+﻿using TheWalkingPets.Service.common;
+
+namespace TheWalkingPets.Service.BLL.Errors.TrabajoErrors
+{
+    public class TrabajoErrors
+    {
+        public static readonly Error NotExists = new(
+            "Trabajo.AlreadyExists",
+            "Trabajo No encontrado");
+
+        public static readonly Error AlreadyExists = new(
+            "Trabajo.AlreadyExists",
+            "La Mascota ya existe");
+
+        public static readonly Error Unhandled = new(
+            "Trabajo.Unhandled",
+            "Error no controlado");
+    }
+}
