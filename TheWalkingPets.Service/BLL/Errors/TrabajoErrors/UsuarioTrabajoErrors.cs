4750c4eed01e13035ae3bd1ac0db61fe4323b1ef
Juan Pablo Ospina <juanpabloospina19@gmail.com>
2025-02-18 22:23:31 -0500

    feat: Add UsuarioTrabajoErrors

---
+++ TheWalkingPets.Service/BLL/Errors/TrabajoErrors/UsuarioTrabajoErrors.cs
@@ -0,0 +1,19 @@
+﻿using TheWalkingPets.Service.common;
+
+namespace TheWalkingPets.Service.BLL.Errors.TrabajoErrors
+{
+    public class UsuarioTrabajoErrors
+    {
+        public static readonly Error NotExists = new(
+            "UsuarioTrabajo.AlreadyExists",
+            "Trabajo No encontrado");
+
+        public static readonly Error AlreadyExists = new(
+            "UsuarioTrabajo.AlreadyExists",
+            "La Mascota ya existe");
+
+        public static readonly Error Unhandled = new(
+            "UsuarioTrabajo.Unhandled",
+            "Error no controlado");
+    }
+}
